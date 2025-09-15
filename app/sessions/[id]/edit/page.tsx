22dbb1ac9b8a5d6d426d93750b1bf9af0ad22826
kechyy <kech3443@gmail.com>
2025-09-15 18:31:10 +0100

    Next bug fix

--- app/sessions/[id]/edit/page.tsx
+++ app/sessions/[id]/edit/page.tsx
@@ -1,10 +1,11 @@
 import SessionForm from "@/components/client/SessionForm";
 
-export default function EditSessionPage({ params }: { params: { id: string } }) {
+export default async function EditSessionPage({ params }:{ params: Promise<{ id: string }> }) {
+  const { id } = await params; 
   return (
     <div className="space-y-6">
       <h1 className="h1">Edit Session</h1>
-      <SessionForm mode="edit" id={params.id} />
+      <SessionForm mode="edit" id={id} />
     </div>
   );
 }
