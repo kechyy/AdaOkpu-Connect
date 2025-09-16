38d79c8b3981c8d14f1f71b45dc8707f75425be4
kechyy <kech3443@gmail.com>
2025-09-16 10:31:40 +0100

    fixed support ledge page broken link

--- components/client/MemberTable.tsx
+++ components/client/MemberTable.tsx
@@ -55,7 +55,7 @@ export default function MemberTable() {
   };
 
   return (
-    <Section title="Current Members" actions={<Link href="/members/new" className="btn">Add New Member</Link>}>
+    <Section title="Current Members" actions={<Link href="/members/new" className="btn">Add Member</Link>}>
       {isLoading ? (
         <div className="p-4 text-sm text-gray-500">Loading…</div>
       ) : members.length === 0 ? (
